6b56809f67ad294213ffac6655528b362ce34072
hyeonki Min <full17@naver.com>
2025-11-12 02:00:58 +0900

    init project

---
+++ src/test/java/min/hyeonki/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package min.hyeonki.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
